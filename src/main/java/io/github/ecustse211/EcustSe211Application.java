c94afde4266101309959475948b1ea8cad52039c
SoltidueMarquez <2630388173@qq.com>
2024-01-05 18:22:15 +0800

    后台代码接口教师表测试
    谁创建项目的时候Springboot版本选的3.2.1，这tm所有的bean位置都变了，查了一个下午，牛魔的给你们一拳

--- src/main/java/io/github/ecustse211/EcustSe211Application.java
+++ src/main/java/io/github/ecustse211/EcustSe211Application.java
@@ -4,7 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages="io.github.ecustse211.mapper")
+@SpringBootApplication
+@MapperScan("io.github.ecustse211.mapper")
 public class EcustSe211Application {
 
 	public static void main(String[] args) {
