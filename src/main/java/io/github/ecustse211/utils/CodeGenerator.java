5f19e5afcc3c6194a4c1c47c5f1b0ecb1964d10f
SoltidueMarquez <2630388173@qq.com>
2024-01-09 16:28:58 +0800

    Attendancerecord表接口实现

--- src/main/java/io/github/ecustse211/utils/CodeGenerator.java
+++ src/main/java/io/github/ecustse211/utils/CodeGenerator.java
@@ -40,7 +40,7 @@ public class CodeGenerator {
                     builder.entityBuilder().enableLombok();
                     builder.controllerBuilder().enableHyphenStyle()  // 开启驼峰转连字符
                             .enableRestStyle();  // 开启生成@RestController 控制器
-                    builder.addInclude("attendance") // 设置需要生成的表名
+                    builder.addInclude("attendancerecord") // 设置需要生成的表名
                             .addTablePrefix("t_", "sys_"); // 设置过滤表前缀
                 })
                 .execute();
